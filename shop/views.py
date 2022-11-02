4153becfd05fbc46d7c6ee42015ab81a0de71777
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-11-02 17:21:06 +0530

    search option completed

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.db.models import Q
 
 
 def index(request, c_slug=None):
@@ -20,3 +21,12 @@ def product_details(request, c_slug, p_slug):
     except Exception as e:
         raise e
     return render(request, 'product.html', {'prod': prod})
+
+
+def search(request):
+    prod = None
+    query = None
+    if "q" in request.GET:
+        query = request.GET.get("q")
+        prod = product.objects.all().filter(Q(name__contains=query) | Q(desc__contains=query))
+    return render(request, 'search.html', {'query': query, 'prod': prod})
