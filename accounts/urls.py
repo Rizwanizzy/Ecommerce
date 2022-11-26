13873f1fa7ba44c1087d5ee4dfb294ea2f4affc8
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-11-26 13:10:25 +0530

    forgot password feature added successfully,but in project settings ; should provide auth emailid and password or temperory password that got from Turning on 2-Step Verification in google accound

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from .import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/',views.logout,name='logout'),
+    #forgot password urls-----------------
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'),name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
 ]
